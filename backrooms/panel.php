9232d2ada06fba969f5290e555efd85193d3ec92
Mike Ehrmantraut <celaakpl85@gmail.com>
2022-11-20 15:37:07 +0100

    t

---
+++ backrooms/panel.php
@@ -0,0 +1,256 @@
+<?php
+session_start();
+if(isset($_POST['logout'])){
+    session_unset();
+    session_destroy();
+}
+if(!isset($_SESSION['user'])){
+    header('location:index.php');
+}
+
+require("bd-authorize.php"); //Autoryzacja dostępu do bazy danych
+
+try{
+    $pdo = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $database . ';port=' . $port, $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $pdo->query('SELECT id_klienta, admin FROM klienci');
+    foreach ($stmt as $row) {
+        if($row['id_klienta'] == $_SESSION['user']){
+            if($row['admin'] != 1){ //dodatkowe bezpieczeństwo
+                session_unset();
+                session_destroy();
+                header('location:index.php');
+            }
+        }
+    }
+    $stmt->closeCursor();
+} catch(PDOException $e) {
+    echo '😵';
+}
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MinersMC</title>
+    <link rel="icon" type="image/png" href="../resources/logo.png">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta&family=Nunito:wght@200;300;400&family=Work+Sans&display=swap" rel="stylesheet">
+
+
+    <style>
+        body{
+            background: repeat url("../resources/bg2.png");
+            color: white;
+            position: relative;
+        }
+        h1{
+            font-family: 'Work Sans', sans-serif;
+        }
+        .separator{
+            margin: 25px 0;
+            background-color: white;
+            border: 0;
+            height: 2px;
+        }
+        .btn-outline-primary{
+            color:black;
+            border-color:#00FF7F;
+            margin:5px;
+            box-shadow:2px 2px 3px #000, inset 2px 2px 3px #000;
+            border-width: 3px;
+        }
+        .btn-outline-primary:hover{
+            box-shadow: 2px 2px 3px #000, inset 0px 0px 0px #000;
+            border-color:#00FF7F;
+            background-color:#00FF7F;
+            color: black;
+        }
+        .navbar-dark .navbar-nav .nav-link{
+            font-family: 'Nunito', sans-serif;
+            font-size: 22px;
+            color: white;
+        }
+        .navbar-dark{
+            background: repeat url("../resources/dirt.jpg");
+        }
+        .nav-item{
+            margin-right:50px;
+        }
+        .scrolled-down{
+            transform:translateY(-100%); transition: all 0.3s ease-in-out;
+        }
+        .scrolled-up{
+            transform:translateY(0); transition: all 0.3s ease-in-out;
+        }
+        .btn-primary{
+            background-color:#00FF7F;
+            color:black;
+            border: none;
+        }
+        .btn-primary:hover{
+            background-color:#00b359;
+            color:black;
+            border: none;
+        }
+        .btn-secondary{
+            background-color:#444;
+            color:white;
+            border: none;
+        }
+        .invis{
+            background-color: rgba(0, 0, 0, 0);
+            border: none;
+        }
+        .big-btn{
+            width: 200px;
+            height: 200px;
+            background-color: #42445A;
+            border-radius: 4px;
+            justify-content: center;
+            display: grid;
+            place-items: center;
+            margin: auto;
+        }
+        .big-btn svg{
+            color: #00FF7F;
+        }
+        .big-btn:hover svg{
+            color:#00b359;
+            width: 105px;
+            height: 105px;
+            transition: 0.3s;
+        }
+        .big-btn:active svg{
+            color:#00b359;
+            width: 95px;
+            height: 95px;
+            transition: 0.2s;
+        }
+        .box-shadow{
+            -webkit-box-shadow: 5px 5px 0px 0px rgba(39, 39, 42, 1);
+            -moz-box-shadow: 5px 5px 0px 0px rgba(39, 39, 42, 1);
+            box-shadow: 5px 5px 0px 0px rgba(39, 39, 42, 1);
+        }
+        .panel-grid{
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            grid-gap: 20px;
+            width: 45%;
+            min-width: 420px;
+            margin-top: 50px;
+            margin-bottom: 50px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .welcomediv{
+            max-width: 50%;
+            margin: auto;
+        }
+        .welcome{
+            margin: 20px auto 20px auto;
+            text-align: center;
+            display: block;
+        }
+    </style>
+
+</head>
+<body data-bs-spy="scroll" data-bs-target="#navigacja">
+
+<div class="container-fluid">
+    <nav class="navbar autohide fixed-top navbar-dark navbar-expand-md justify-content-center">
+        <div class="container">
+            <a href=".." class="navbar-brand d-flex w-50 me-auto"><img src="../resources/logo.png" style="height:50px;" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+                <ul class="navbar-nav w-100 justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="..">Powrót</a>
+                    </li>
+                    <li class="nav-item">
+                        <form method="post">
+                            <button type="submit" name="logout" class="invis">
+                                <a class="nav-link" >Wyloguj się</a>
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+                <div class="nav navbar-nav ms-auto w-100"></div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="welcomediv">
+        <?php
+        $time = time();
+        try{
+            $pdo = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $database . ';port=' . $port, $username, $password);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $pdo->query('SELECT nick FROM klienci WHERE id_klienta LIKE "'.$_SESSION['user'].'"');
+            foreach ($stmt as $row) {
+                if(date('H', $time) > 18 || date('H', $time) < 5) {
+                    echo "<h1 class='welcome'>Dobry wieczór, " . $row['nick'] . "</h1>";
+                } else {
+                    echo "<h1 class='welcome'>Dzień dobry, " . $row['nick'] . "</h1>";
+                }
+                echo "<img src='https://minotar.net/helm/".$row['nick']."/100.png' class='welcome box-shadow' />";
+            }
+            $stmt->closeCursor();
+        } catch(PDOException $e) {
+            echo '😵';
+        }
+        ?>
+    </div>
+    <div class="separator"></div>
+
+    <div class="panel-grid"> <!--tutaj będzie dodany tekst który wyświetla nazwę zakładki na którą się najeżdża-->
+        <a class="big-btn box-shadow" href="panel-users.php"> <!--KLIENCI-->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16" width="100" height="100">
+                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"></path>
+            </svg>
+        </a>
+        <a class="big-btn box-shadow"> <!--PROMOCJE-->
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
+                <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0zM4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+            </svg>
+        </a>
+        <a class="big-btn box-shadow"> <!--PRODUKTY-->
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383L5.93 1.757zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1z"></path>
+            </svg>
+        </a>
+        <a class="big-btn box-shadow"> <!--ZAMÓWIENIA-->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16" height="100" width="100">
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
+                <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"></path>
+            </svg>
+        </a>
+        <a class="big-btn box-shadow"> <!--DODAJ WPIS-->
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-chat-right-quote-fill" viewBox="0 0 16 16">
+                <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM7.194 4.766c.087.124.163.26.227.401.428.948.393 2.377-.942 3.706a.446.446 0 0 1-.612.01.405.405 0 0 1-.011-.59c.419-.416.672-.831.809-1.22-.269.165-.588.26-.93.26C4.775 7.333 4 6.587 4 5.667 4 4.747 4.776 4 5.734 4c.271 0 .528.06.756.166l.008.004c.169.07.327.182.469.324.085.083.161.174.227.272zM11 7.073c-.269.165-.588.26-.93.26-.958 0-1.735-.746-1.735-1.666 0-.92.777-1.667 1.734-1.667.271 0 .528.06.756.166l.008.004c.17.07.327.182.469.324.085.083.161.174.227.272.087.124.164.26.228.401.428.948.392 2.377-.942 3.706a.446.446 0 0 1-.613.01.405.405 0 0 1-.011-.59c.42-.416.672-.831.81-1.22z"></path>
+            </svg>
+        </a>
+        <a class="big-btn box-shadow"> <!--ZARZĄDZAJ WPISAMI-->
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-file-earmark-font" viewBox="0 0 16 16">
+                <path d="M10.943 6H5.057L5 8h.5c.18-1.096.356-1.192 1.694-1.235l.293-.01v5.09c0 .47-.1.582-.898.655v.5H9.41v-.5c-.803-.073-.903-.184-.903-.654V6.755l.298.01c1.338.043 1.514.14 1.694 1.235h.5l-.057-2z"></path>
+                <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"></path>
+            </svg>
+        </a>
+    </div>
+    <div style="text-align:center;color:white;">Wdrożenie - AM 2022</div>
+</div>
+<script src="../resources/scroll.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
